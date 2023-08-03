484d1629558a47de00069b4e31b5afb20a8d9548
Cylene Santos <141143291+Cymama@users.noreply.github.com>
2023-08-03 10:20:10 -0300

    Criamos rotas personalizadas

--- app/routes.py
+++ app/routes.py
@@ -3,11 +3,12 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template ('index.html', titulo = 'Página inicial')
+    return render_template ('index.html', titulo = 'Página imicial')
 
-@app.route('/projetos')
-def projetos():
-    return render_template ('projetos.html', titulo = 'Projetos')
+@app.route('/contatos')
+def contatos():
+    return render_template ('contatos.html', titulo = 'Contatos')
 
-
-
+@app.route('/sobre')
+def sobre():
+    return render_template ('sobre.html', titulo = 'Sobre')
